3509b9d0278761f8fe029884e7b006747e7fa515
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-07-29 16:19:47 +0100

    add Autocompleter , implementation of OpenDocumentHandler to take documents and create words Trie

---
+++ src/main/java/org/mql/autocompletionplugin/Autocompleter.java
@@ -0,0 +1,53 @@
+package org.mql.autocompletionplugin;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.StyledDocument;
+
+import org.mql.jcodeeditor.documentHandlers.OpenDocumentsHandler;
+
+public class Autocompleter implements OpenDocumentsHandler {
+	private List<StyledDocument> openDocuments;
+	private Node root;
+	
+	public Autocompleter() {
+		root =  new Node();
+	}
+
+	@Override
+	public void setDocuments(List<StyledDocument> openDocuments) {
+		this.openDocuments = openDocuments;
+		// TODO create a set of words 
+		for (StyledDocument d : openDocuments) {
+			// TODO the listener should take the collection of words to add to it new words and test 
+			try {
+				String text = d.getText(0, d.getLength());
+//				words.addAll(Arrays.asList());
+				String words[] = text.split("\\b\\w*\\b");
+				for(String w : words) {
+					root.addChild(w.charAt(0), new Node(w.substring(1)));
+				}
+				
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println(root);
+
+		
+	}
+
+	@Override
+	public void execute() {
+		// TODO i need to create a DocumentListener and add it to docs here
+//		for (StyledDocument d : openDocuments) {
+//			// TODO the listener should take the collection of words to add to it new words and test 
+//			d.addDocumentListener(null);
+//		}
+	}
+
+}
--- src/main/java/org/mql/autocompletionplugin/Node.java
+++ src/main/java/org/mql/autocompletionplugin/Node.java
@@ -3,6 +3,7 @@ package org.mql.autocompletionplugin;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Vector;
 
 /*
@@ -21,7 +22,7 @@ public class Node {
 		super();
 		if (s.length() >= 2) {
 			addChild(s.charAt(0), new Node(s.substring(1)));
-		}else if(s.length() == 1){
+		} else if (s.length() == 1) {
 			this.isLeaf = true;
 		}
 	}
@@ -50,6 +51,20 @@ public class Node {
 
 	public void setIsLeaf(boolean isLeaf) {
 		this.isLeaf = isLeaf;
+	}
+
+	@Override
+	public String toString() {
+		String s = "";
+
+		for (Entry<Character, List<Node>> entry : children.entrySet()) {
+			s += "key : " + entry.getKey();
+			for (Node n : children.get(entry.getKey())) {
+				s += n.toString();
+			}
+			s += "\n";
+		}
+		return s;
 	}
 
 }
