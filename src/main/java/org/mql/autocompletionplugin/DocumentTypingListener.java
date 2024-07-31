b78107384e02dd65b748b2d05173974e094faeb3
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-07-31 16:54:59 +0100

    fix: only give suggestion when the cursor is still on the word being typed and not when there is spaces after it

--- src/main/java/org/mql/autocompletionplugin/DocumentTypingListener.java
+++ src/main/java/org/mql/autocompletionplugin/DocumentTypingListener.java
@@ -11,32 +11,33 @@ import javax.swing.text.Document;
 public class DocumentTypingListener implements DocumentListener {
 	private Node root;
 	private Pattern p = Pattern.compile("\\b\\w+\\b");
-	
+
 	public DocumentTypingListener(Node root) {
 		this.root = root;
 	}
 
 	@Override
 	public void insertUpdate(DocumentEvent e) {
-		 try {
+		try {
 			Document doc = e.getDocument();
 			String text = doc.getText(0, e.getOffset() + e.getLength());
-			System.out.println("text from 0 to change :");
-			System.out.println(text);
-			// 1 = get the offset of the change
-			// 2 = get the offset of last word boundry \b from 0 to change offset
-			// get the word from 1 to 2
-			// get suggestions
-			Matcher m = p.matcher(text);
-			String typedWord = "";
-			while(m.find()) {
-				typedWord = m.group();
+			String insertion = doc.getText(e.getOffset(), e.getLength());
+			char c = insertion.charAt(insertion.length() - 1);
+			//
+			if (c != ' ') {
+
+				Matcher m = p.matcher(text);
+				String typedWord = "";
+				while (m.find()) {
+					typedWord = m.group();
+				}
+				System.out.println("typedWord : " + typedWord);
+				System.out.println("suggestions for typed word :");
+				System.out.println(root.getSuggestions(typedWord));
+				// i need to add new word after a suggestion is inserted or after each complete
+				// word \b
+				// root.insert(typedWord);
 			}
-			System.out.println("typedWord : " + typedWord);
-			System.out.println("suggestions for typed word :");
-			System.out.println(root.getSuggestions(typedWord));
-			// i need to add new word after a suggestion is inserted or after each complete word \b
-//			root.insert(typedWord);
 		} catch (BadLocationException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -45,7 +46,35 @@ public class DocumentTypingListener implements DocumentListener {
 
 	@Override
 	public void removeUpdate(DocumentEvent e) {
+		try {
+			Document doc = e.getDocument();
+			String text = doc.getText(0, e.getOffset() - e.getLength());
+			System.out.println("text from 0 to change :");
+			System.out.println(text);
 
+			// test if cursor is on a word not on a space
+			String insertion = doc.getText(e.getOffset() - e.getLength(), e.getLength());
+			char c = insertion.charAt(insertion.length() - 1);
+			if (c != ' ') {
+				Matcher m = p.matcher(text);
+				String typedWord = "";
+				while (m.find()) {
+					typedWord = m.group();
+				}
+				System.out.println("typedWord : " + typedWord);
+				System.out.println("suggestions for typed word :");
+				System.out.println(root.getSuggestions(typedWord));
+				//
+
+				// i need to add new word after a suggestion is inserted or after each complete
+				// word \b
+				// root.insert(typedWord);
+			}
+
+		} catch (BadLocationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 
 	@Override
