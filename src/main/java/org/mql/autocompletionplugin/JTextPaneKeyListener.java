ae0ceb89aa693c243e5466ae43d96cfb847e2518
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-02 15:08:15 +0100

    hide suggestions when escape is pressed

--- src/main/java/org/mql/autocompletionplugin/JTextPaneKeyListener.java
+++ src/main/java/org/mql/autocompletionplugin/JTextPaneKeyListener.java
@@ -16,26 +16,13 @@ public class JTextPaneKeyListener implements KeyListener {
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-//		if (e.getKeyCode() == KeyEvent.SHIFT_DOWN_MASK) {
-//			System.out.println("shift pressed");
-////			if(selected == -1) {
-//			selected++;
-//			if (popupMenu.isVisible()) {
-//				popupMenu.getSelectionModel().setSelectedIndex(selected);
-//				popupMenu.requestFocusInWindow();
-//				popupMenu.getComponent(selected).requestFocus();
-//			}
-////			}
-//
-//		}
-
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE && popupMenu.isVisible()) {
+			popupMenu.setVisible(false);
+		}
 		if (e.getKeyCode() == KeyEvent.VK_TAB) {
 			e.consume(); // Prevent default tab behavior
 			if (popupMenu.isVisible()) {
+				
 				System.out.println("shift pressed");
 				selected++;
 				if (popupMenu.isVisible()) {
@@ -49,8 +36,13 @@ public class JTextPaneKeyListener implements KeyListener {
 	}
 
 	@Override
+	public void keyPressed(KeyEvent e) {
+		
+
+	}
+
+	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
