8c3c1c91ebc5b41a94b7430ea5be7a403ec9140d
laktam abdellah <laktamlaktamabdellah@gmail.com>
2024-08-03 19:02:25 +0100

    change background of selected item

--- src/main/java/org/mql/autocompletionplugin/JTextPaneKeyListener.java
+++ src/main/java/org/mql/autocompletionplugin/JTextPaneKeyListener.java
@@ -1,10 +1,14 @@
 package org.mql.autocompletionplugin;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.KeyboardFocusManager;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.UIManager;
 
 public class JTextPaneKeyListener implements KeyListener {
 	private JPopupMenu popupMenu;
@@ -25,21 +29,20 @@ public class JTextPaneKeyListener implements KeyListener {
 		if (e.getKeyCode() == KeyEvent.VK_ESCAPE && popupMenu.isVisible()) {
 			popupMenu.setVisible(false);
 		}
+		// if the popupmenu is visible dont use arrow mouvement in the doc
 		// if popup is visible that mean there is items
-		if (e.getKeyCode() == KeyEvent.VK_UP && popupMenu.isVisible() && selected > 0 ) {
-			selected--;
-			e.consume(); // Prevent default tab behavior
-			// KeyboardFocusManager.getCurrentKeyboardFocusManager().clearGlobalFocusOwner();
-			System.out.println("up pressed");
-			popupMenu.getSelectionModel().setSelectedIndex(selected);
-
-		}
-		if (e.getKeyCode() == KeyEvent.VK_DOWN && popupMenu.isVisible() && selected < popupMenu.getComponentCount() - 1 ) {
-			selected++;
-			e.consume(); // Prevent default tab behavior
-			// KeyboardFocusManager.getCurrentKeyboardFocusManager().clearGlobalFocusOwner();
-			System.out.println("down pressed");
-			popupMenu.getSelectionModel().setSelectedIndex(selected);
+		if (popupMenu.isVisible()) {
+			e.consume(); // Prevent default tab behaviors
+			if (e.getKeyCode() == KeyEvent.VK_UP && selected > 0) {
+				selected--;
+				System.out.println("up pressed");
+			}
+			if (e.getKeyCode() == KeyEvent.VK_DOWN && selected < popupMenu.getComponentCount() - 1) {
+				selected++;
+				e.consume();
+				System.out.println("down pressed");
+			}
+			selectItem(selected);
 
 		}
 
@@ -50,4 +53,19 @@ public class JTextPaneKeyListener implements KeyListener {
 
 	}
 
+	private void selectItem(int selected) {
+		popupMenu.getSelectionModel().setSelectedIndex(selected);
+		System.out.println("selected : " + popupMenu.getComponent(popupMenu.getSelectionModel().getSelectedIndex()));
+		int itemCount = popupMenu.getComponentCount();
+		for(int item = 0; item < itemCount; item++) {
+			JMenuItem menuItem = (JMenuItem) popupMenu.getComponent(item);
+			if(item == selected) {
+				menuItem.setBackground(Color.BLUE);
+			}else {
+				menuItem.setBackground(UIManager.getColor("MenuItem.background"));
+			}
+		}
+		popupMenu.repaint();
+
+	}
 }
